e7b395720d8f5e8bd87f4c0b0ba8ca21f946e020
Biianca16 <160866107+Biianca16@users.noreply.github.com>
2024-03-08 10:44:39 -0300

    Add files via upload

---
+++ Trabalho 2 INFO/questao4.py
@@ -0,0 +1,12 @@
+a = float(input("Digite o primeiro valor: "))                 
+b = float(input("Digite o segundo valor: "))
+c = float(input("Digite o terceiro valor: "))
+
+# Usar propriedade distributiva para ordenar os valores
+if a > b:
+    a, b = b, a
+if b > c:
+    b, c = c, b
+
+# Apresentar os valores em ordem crescente
+print("Os valores em ordem crescente são:", a, b, c)
